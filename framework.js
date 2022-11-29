2a6713a6dd209f12e88c06f4ad919c1b9bcd1501
nico b <bouvet.nicolas@gmail.com>
2022-11-29 12:26:45 +0100

    build framework

--- framework.js
+++ framework.js
@@ -1,8 +1,227 @@
 // assets/shaders/phong.vert
 
+precision highp float;
+//precision highp int;
+
+attribute vec3 aPosition;
+attribute vec3 aNormal;
+attribute vec2 aTexCoord;
+
+uniform vec3 uAmbientColor[5];
+
+uniform mat4 uModelViewMatrix;
+uniform mat4 uProjectionMatrix;
+uniform mat3 uNormalMatrix;
+uniform int uAmbientLightCount;
+
+varying vec3 vNormal;
+varying vec2 vTexCoord;
+varying vec3 vViewPosition;
+varying vec3 vAmbientColor;
+
+void main() {
+    vec4 viewModelPosition = uModelViewMatrix * vec4(aPosition, 1.0);
+
+    // Pass varyings to fragment shader
+    vViewPosition = viewModelPosition.xyz;
+    gl_Position = uProjectionMatrix * viewModelPosition;  
+
+    vNormal = uNormalMatrix * aNormal;
+    vTexCoord = aTexCoord;
+
+    // TODO: this should be a uniform
+    vAmbientColor = vec3(0.0);
+
+    for (int i = 0; i < 5; i++) {
+        if (i < uAmbientLightCount) {
+            vAmbientColor += uAmbientColor[i];
+        }
+    }
+}
 
 // assets/shaders/phong.frag
 
+precision highp float;
+//precision highp int;
+
+uniform mat4 uViewMatrix;
+
+uniform bool uUseLighting;
+
+uniform int uAmbientLightCount;
+uniform vec3 uAmbientColor[5];
+
+uniform int uDirectionalLightCount;
+uniform vec3 uLightingDirection[5];
+uniform vec3 uDirectionalDiffuseColors[5];
+uniform vec3 uDirectionalSpecularColors[5];
+
+uniform int uPointLightCount;
+uniform vec3 uPointLightLocation[5];
+uniform vec3 uPointLightDiffuseColors[5];	
+uniform vec3 uPointLightSpecularColors[5];
+
+uniform int uSpotLightCount;
+uniform float uSpotLightAngle[5];
+uniform float uSpotLightConc[5];
+uniform vec3 uSpotLightDiffuseColors[5];
+uniform vec3 uSpotLightSpecularColors[5];
+uniform vec3 uSpotLightLocation[5];
+uniform vec3 uSpotLightDirection[5];
+
+uniform bool uSpecular;
+uniform float uShininess;
+
+uniform float uConstantAttenuation;
+uniform float uLinearAttenuation;
+uniform float uQuadraticAttenuation;
+
+const float specularFactor = 2.0;
+const float diffuseFactor = 0.73;
+
+struct LightResult {
+    float specular;
+    float diffuse;
+};
+
+float _phongSpecular(vec3 lightDirection, vec3 viewDirection, vec3 surfaceNormal, float shininess) {
+    vec3 R = reflect(lightDirection, surfaceNormal);
+    return pow(max(0.0, dot(R, viewDirection)), shininess);
+}
+
+float _lambertDiffuse(vec3 lightDirection, vec3 surfaceNormal) {
+    return max(0.0, dot(-lightDirection, surfaceNormal));
+}
+
+LightResult _light(vec3 viewDirection, vec3 normal, vec3 lightVector) {
+    vec3 lightDir = normalize(lightVector);
+
+    //compute our diffuse & specular terms
+    LightResult lr;
+
+    if (uSpecular) {
+        lr.specular = _phongSpecular(lightDir, viewDirection, normal, uShininess);
+    }
+
+    lr.diffuse = _lambertDiffuse(lightDir, normal);
+
+    return lr;
+}
+
+void totalLight(
+  vec3 modelPosition,
+  vec3 normal,
+  out vec3 totalDiffuse,
+  out vec3 totalSpecular
+) {
+
+  totalSpecular = vec3(0.0);
+
+  if (!uUseLighting) {
+    totalDiffuse = vec3(1.0);
+    return;
+  }
+
+  totalDiffuse = vec3(0.0);
+
+  vec3 viewDirection = normalize(-modelPosition);
+
+  for (int j = 0; j < 5; j++) {
+    if (j < uDirectionalLightCount) {
+      vec3 lightVector = (uViewMatrix * vec4(uLightingDirection[j], 0.0)).xyz;
+      vec3 lightColor = uDirectionalDiffuseColors[j];
+      vec3 specularColor = uDirectionalSpecularColors[j];
+      LightResult result = _light(viewDirection, normal, lightVector);
+      totalDiffuse += result.diffuse * lightColor;
+      totalSpecular += result.specular * lightColor * specularColor;
+    }
+
+    if (j < uPointLightCount) {
+      vec3 lightPosition = (uViewMatrix * vec4(uPointLightLocation[j], 1.0)).xyz;
+      vec3 lightVector = modelPosition - lightPosition;
+    
+      //calculate attenuation
+      float lightDistance = length(lightVector);
+      float lightFalloff = 1.0 / (uConstantAttenuation + lightDistance * uLinearAttenuation + (lightDistance * lightDistance) * uQuadraticAttenuation);
+      vec3 lightColor = lightFalloff * uPointLightDiffuseColors[j];
+      vec3 specularColor = lightFalloff * uPointLightSpecularColors[j];
+
+      LightResult result = _light(viewDirection, normal, lightVector);
+      totalDiffuse += result.diffuse * lightColor;
+      totalSpecular += result.specular * lightColor * specularColor;
+    }
+
+    if(j < uSpotLightCount) {
+      vec3 lightPosition = (uViewMatrix * vec4(uSpotLightLocation[j], 1.0)).xyz;
+      vec3 lightVector = modelPosition - lightPosition;
+    
+      float lightDistance = length(lightVector);
+      float lightFalloff = 1.0 / (uConstantAttenuation + lightDistance * uLinearAttenuation + (lightDistance * lightDistance) * uQuadraticAttenuation);
+
+      vec3 lightDirection = (uViewMatrix * vec4(uSpotLightDirection[j], 0.0)).xyz;
+      float spotDot = dot(normalize(lightVector), normalize(lightDirection));
+      float spotFalloff;
+      if(spotDot < uSpotLightAngle[j]) {
+        spotFalloff = 0.0;
+      }
+      else {
+        spotFalloff = pow(spotDot, uSpotLightConc[j]);
+      }
+      lightFalloff *= spotFalloff;
+
+      vec3 lightColor = uSpotLightDiffuseColors[j];
+      vec3 specularColor = uSpotLightSpecularColors[j];
+     
+      LightResult result = _light(viewDirection, normal, lightVector);
+      
+      totalDiffuse += result.diffuse * lightColor * lightFalloff;
+      totalSpecular += result.specular * lightColor * specularColor * lightFalloff;
+    }
+  }
+
+  totalDiffuse *= diffuseFactor;
+  totalSpecular *= specularFactor;
+}
+
+uniform vec3 uFogColor;
+uniform float uFogStart;
+uniform float uFogEnd;
+
+float fog_linear(float dist, float start, float end) {
+    return 1.0 - clamp((end - dist) / (end - start), 0.0, 1.0);
+}
+
+uniform vec4 uSpecularMatColor;
+uniform vec4 uAmbientMatColor;
+uniform vec4 uEmissiveMatColor;
+
+uniform vec4 uMaterialColor;
+uniform vec4 uTint;
+uniform sampler2D uSampler;
+uniform bool isTexture;
+
+varying vec3 vNormal;
+varying vec2 vTexCoord;
+varying vec3 vViewPosition;
+varying vec3 vAmbientColor;
+
+void main(void) {
+    vec3 diffuse;
+    vec3 specular;
+    totalLight(vViewPosition, normalize(vNormal), diffuse, specular);
+
+    // Calculating final color as result of all lights (plus emissive term).
+    gl_FragColor = isTexture ? texture2D(uSampler, vTexCoord) * (uTint / vec4(255, 255, 255, 255)) : uMaterialColor;
+    gl_FragColor.rgb = diffuse * gl_FragColor.rgb + 
+                    vAmbientColor * uAmbientMatColor.rgb + 
+                    specular * uSpecularMatColor.rgb + 
+                    uEmissiveMatColor.rgb;
+
+    // fog
+    float fogDistance = gl_FragCoord.z / gl_FragCoord.w;
+    float fogAmount = fog_linear(fogDistance, uFogStart, uFogEnd);
+    gl_FragColor.rgb = mix(gl_FragColor.rgb, uFogColor, fogAmount);
+}
 
 // easings.js
 
